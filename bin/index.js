cb8567308090d0e147c97d92bd7e4b0c008cee20
JuanSierra <jksierra@gmail.com>
2022-10-22 11:58:42 +0100

    Initial commit with basic setup

---
+++ bin/index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const { Command } = require('commander');
+const lib = require('../lib/main');
+
+const program = new Command();
+
+function myParseInt(value, dummyPrevious) {
+  // parseInt takes a string and a radix
+  const parsedValue = parseInt(value, 10);
+  if (isNaN(parsedValue)) {
+    throw new commander.InvalidArgumentError('Not a number.');
+  }
+  return parsedValue;
+}
+
+program
+  .command('process')
+  .argument('<first>', 'author name')
+  //.argument('[second]', 'integer argument', myParseInt, 1000)
+  .action((author) => {
+	lib.runProcess(author)
+  });
+
+program.parse();
